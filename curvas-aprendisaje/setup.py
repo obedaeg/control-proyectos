5f58916305daa788374278146b453966edc26df3
Obed Espinoza <obed.espinoza@healthcare.com>
2019-08-21 20:52:21 -0600

    Subiendo primera version de Algoritmo de curvas de aprendisaje

---
+++ curvas-aprendisaje/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='curvas-aprendisaje',
+    version='1.0',
+    packages=['jupyter',
+              'xlrd',
+              'pandas',
+              'sklearn',
+              'matplotlib'],
+    url='',
+    license='',
+    author='obed',
+    author_email='obedaeg@gmail.com',
+    description=''
+)
